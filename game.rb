0776743370e7cb53cd425094c82d401fa87bb08b
Eli Smukler <elismukler@gmail.com>
2015-04-01 11:36:40 -0400

    organized files into lib folder

--- game.rb
+++ game.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
-require_relative 'chess_board'
-require_relative 'computer_player'
+require_relative 'lib/chess_board'
+require_relative 'lib/computer_player'
 
 class Game
   attr_reader :board, :player1, :player2
